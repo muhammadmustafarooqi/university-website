d6337da832601857e064fa08da76760f6cef45d4
Muhammad Mustafa Hussain <140386879+muhammadmustafarooqi@users.noreply.github.com>
2024-11-17 20:53:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// const menuBtn = document.getElementById('menu-btn');
+// const navBar = document.getElementById('navbar-default');
+
+// // Toggle the navbar visibility
+// menuBtn.addEventListener('click', () => {
+//     navBar.classList.toggle('hidden'); // Show/hide the menu
+//     document.body.classList.toggle('overflow-hidden'); // Prevent/allow scrolling
+// });
+// document.addEventListener('keydown', (e) => {
+//     if (e.key === 'Escape' && !navBar.classList.contains('hidden')) {
+//         navBar.classList.add('hidden'); // Hide the menu
+//         document.body.classList.remove('overflow-hidden'); // Allow scrolling
+//     }
+// });
